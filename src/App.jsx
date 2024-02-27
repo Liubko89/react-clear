5a05d629de2d936080bde0c16b3c934c9945c28c
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-02-27 22:42:09 +0000

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
-import "./App.css";
-import FriendList from "./components/FriendList/FriendList";
-import Profile from "./components/Profile/Profile";
-import userData from "./userData.json";
-import friends from "./friends.json";
-import transactions from "./transactions.json";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-
-const App = () => {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory transactions={transactions} />
-    </>
-  );
-};
+function App() {
+  return <div></div>;
+}
 
 export default App;
